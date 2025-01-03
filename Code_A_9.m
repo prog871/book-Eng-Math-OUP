7c07b2ffe76f0d6552ed4f2008a0a109ec76e10c
prog871 <165429386+prog871@users.noreply.github.com>
2025-01-03 21:25:17 +0000

    Delete Code_A_9.m

--- Code_A_9.m
+++
@@ -1,9 +0,0 @@
-% find the integral of function f(x) = 3x^4+5x^2-3
-clear all; clc; close all;
-% define symbolic x
-syms x
-% define function
-f=3*x^4+5*x^2-3;
-% calculate the integral
-int(f,x,-2,8)
-
