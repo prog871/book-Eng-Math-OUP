27f5d03904f30bf8f6e9eead4c44720d6c0c6148
prog871 <165429386+prog871@users.noreply.github.com>
2025-01-03 21:25:09 +0000

    Delete Code_A_8.m

--- Code_A_8.m
+++
@@ -1,9 +0,0 @@
-% find the derivative of function f(x)= -5 x^3+2 x^2+12 x-sin (x). 
-clear all; clc; close all;
-% define symbolic x
-syms x
-% define function
-f=-5*x^3+2*x^2+12*x-sin(x);
-% calculate first derivative
-diff(f,x)
-
